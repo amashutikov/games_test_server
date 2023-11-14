bc77e834ee39bdbf4dac089cff09d0b3dbb776c3
Andrii Mashutikov <mashutikov.a.o@gmail.com>
2023-11-15 00:13:11 +0200

    Added vercel config

--- src/index.js
+++ src/index.js
@@ -22,7 +22,10 @@ database.once('connected', () => {
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(authRouter);
