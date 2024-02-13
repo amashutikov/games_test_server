b6696c5445589db24ba025c56e0e5fc04ffc2cf6
amashutikov <mashutikov.a.o@gmail.com>
2024-02-13 14:37:06 +0200

    discarded prev commit

--- src/index.js
+++ src/index.js
@@ -32,14 +32,6 @@ app.use(
     credentials: true,
   })
 );
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
 app.use(express.json());
 app.use(cookieParser());
 app.use(authRouter);
