37e1117cc81deef0761e722c4284c5aa688e85c1
amashutikov <mashutikov.a.o@gmail.com>
2024-03-06 17:06:38 +0200

    added IGDB integration

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,6 +1,46 @@
 import { ApiError } from '../exeptions/api.error.js';
 import { gameService } from '../services/game.service.js';
 
+const DATA_URL = 'https://api.igdb.com/v4';
+const CLIENT_ID = process.env.IGDB_APP_CLIENT_ID;
+const ACCESS_TOKEN = process.env.IGDB_APP_ACCESS_TOKEN;
+
+function wait(delay) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+}
+
+async function request(method, data, endpoint) {
+  const options = { method };
+
+  if (data) {
+    options.body = data;
+    // Remove 'no-cors' mode
+    options.headers = {
+      'Client-ID': CLIENT_ID,
+      'Content-Type': 'text/plain',
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+      origin: 'https://localhost:3006',
+    };
+  }
+
+  await wait(300);
+
+  // Use a CORS proxy if needed
+  const proxyUrl = 'https://cors-anywhere-34sl.onrender.com/';
+  const urlWithProxy = `${proxyUrl}${DATA_URL}${endpoint}`;
+
+  const response = await fetch(urlWithProxy, options);
+
+  if (!response.ok) {
+    const errorText = await response.text();
+    console.error('Error:', errorText);
+    throw new Error();
+  }
+  return await response.json();
+}
+
 const getTopRatedGames = async (req, res) => {
   const { numberOfGames } = req.body;
 
@@ -10,9 +50,17 @@ const getTopRatedGames = async (req, res) => {
 
   const count = await gameService.getNumberOfGames();
 
-  const topGames = (await gameService.getTopRatedGames(numberOfGames))
+  const topGameIds = (await gameService.getTopRatedGames(numberOfGames))
     .map((game) => game.id)
     .sort((a, b) => b.totalRating - a.totalRating);
+
+  const topGames = await request(
+    'POST',
+    `fields name, summary, id, slug, artworks.*, cover.*;
+        limit ${numberOfGames};
+        where id = (${topGameIds.join(',')});`,
+    '/games'
+  );
 
   res.status(200).send({ games: topGames, count });
 };
